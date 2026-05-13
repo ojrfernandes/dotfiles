7ef5d023a41f8cbd6ac7f4cdfd34c820beb93607
ojrfernandes <jose.r.fernandes.j@gmail.com>
2026-05-13 11:30:04 +0200

    fixed Zoom/Meet screen sharing issue in hyprland

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -52,6 +52,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("wl-paste --type text --watch cliphist store")
     hl.exec_cmd("waybar & swaync & hypridle & hyprpaper")
     hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+    hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 end)
 
 -------------------------------
